56f3f9f3bffd8fc7768a2aba584f130ffaac1149
yamboni1 <ncyambot.work@gmail.com>
2023-08-15 21:57:33 +0800

    chore: added decodeToItems, calculatePerItem and calculateTotalCost

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,39 @@
 package pos.machine;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
-        return null;
+
+        List<ReceiptItem> receiptItems =decodeToItems(barcodes);
+        Receipt receipt = calculateCost(receiptItems);
+        return renderReceipt(receipt);
+
+    }
+    public List<ReceiptItem> decodeToItems(List<String> barcodes){
+        List<Item> items = ItemsLoader.loadAllItems();
+        List<ReceiptItem> receiptItems =new ArrayList<>();
+        List<String> specificBarcodes = barcodes.stream().distinct().collect(Collectors.toList());
+        //count of barcodes
+        HashMap<String, Integer> countOfItems = new HashMap<String, Integer>();
+        specificBarcodes.forEach(barcode -> countOfItems.put(barcode, Collections.frequency(barcodes,barcode)));
+
+        items.forEach(item -> {
+            ReceiptItem receiptItem = new ReceiptItem();
+            receiptItem.setName(item.getName());
+            receiptItem.setUnitPrice(item.getPrice());
+            receiptItem.setQuantity(countOfItems.get(item.getBarcode()));
+            receiptItems.add(receiptItem);
+        });
+        return receiptItems;
+    }
+    public List<ReceiptItem> calculatePerItem(List<ReceiptItem> receiptItems) {
+        receiptItems.forEach(receiptItem -> receiptItem.setSubTotal(receiptItem.getQuantity() * receiptItem.getUnitPrice()));
+        return receiptItems;
+    }
+
+    public int calculateTotalPrice(List<ReceiptItem> receiptItems) {
+        return receiptItems.stream().mapToInt(ReceiptItem::getSubTotal).sum();
     }
 }
